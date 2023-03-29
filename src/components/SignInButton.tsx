0223cbe023143e8f4162b8684aa2a6406f6f168e
kirk admin <kirkalainandes@gmail.com>
2023-03-29 15:11:40 +0800

    add dashboard page and logic

--- src/components/SignInButton.tsx
+++ src/components/SignInButton.tsx
@@ -3,7 +3,7 @@
 import { signIn } from "next-auth/react";
 import React, { FC, useState } from "react";
 import { Button } from "./ui/Button";
-import { toast } from "@/ui/Toast";
+import { toast } from "@/components/ui/toast";
 
 export interface SignInButtonProps {}
 const SignInButton: FC<SignInButtonProps> = () => {
--- src/components/SignOutButton.tsx
+++ src/components/SignOutButton.tsx
@@ -3,7 +3,7 @@
 import React, { FC, useState } from "react";
 import { Button } from "@/ui/Button";
 import { signOut } from "next-auth/react";
-import { toast } from "@/ui/Toast";
+import { toast } from "@/components/ui/toast";
 
 export interface SignOutButtonProps {}
 const SignOutButton: FC<SignOutButtonProps> = () => {
