fdf3f5cb98babb09315d3ec2914e98a65e76a6b9
kirk admin <kirkalainandes@gmail.com>
2023-03-29 11:52:34 +0800

    initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import "@/styles/globals.css";
+import { Inter } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html
+      lang="en"
+      className={cn("bg-white text-slate-900 antialiased", inter)}
+    >
+      <body className="min-h-screen bg-slate-50 dark:bg-slate-900 antialiased">
+        {children}
+      </body>
+    </html>
+  );
+}
